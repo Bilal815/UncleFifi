f0f35e0fcfbe1c93a5c4829f43c11ab787340a92
Felipe Ferreira <felipedferreira11@gmail.com>
2020-08-13 01:49:02 -0400

    issue debugging...

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component ,useState } from 'react'
 import moment from 'moment'
-import SingleDate from '../Components/SingleDate'
+import SingleDate from './../Components/SingleDate'
 
 class MainPage extends Component
 {
@@ -9,22 +9,43 @@ class MainPage extends Component
         // STATE
         this.state = { 
             startDate: moment(),// this means today in Moment
-            focused: false
+            focused: false,
+            count: 0
          }
     }
 
     // METHODS
-    toggleFocus = () => this.setState({focused: !this.state.focused})
+    toggleFocus = () => {
+        this.setState({
+            focused: !this.state.focused,
+            count: this.state.count + 1
+        })
+    }
     onDateChange = newDate => this.setState({startDate: newDate})
 
 
     render() {
-        return (<SingleDate 
-                date={this.state.startDate}
-                focused={this.state.focused}
-                onDateChange={this.onDateChange}
-                toggleFocus={this.toggleFocus}
-            />)
+        return (<div className="container">
+            <div className="row">
+                <SingleDate 
+                        date={this.state.startDate}
+                        focused={this.state.focused}
+                        onDateChange={this.onDateChange}
+                        toggleFocus={this.toggleFocus}
+                />
+            </div>
+            <div className="row">
+                <div>
+                    <span>Start Date</span><span>{this.state.startDate.format('L')}</span>
+                </div>
+                <div>
+                    <span>Focused State</span><span> {this.state.focused ? "TRUE" : "FALSE"} </span>
+                </div>
+                <div>
+                    <span>Click Count</span><span> {this.state.count} </span>
+                </div>
+            </div>
+            </div>)
     }
 
 }
